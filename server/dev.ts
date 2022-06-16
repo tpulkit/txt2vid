4e853670d0f1c9c177ced82a0cbd7323b08f2408
101arrowz <arjunbarrett@gmail.com>
2022-06-16 12:14:28 -0700

    patches

--- server/dev.ts
+++ server/dev.ts
@@ -2,6 +2,9 @@ import api from '.';
 import express from 'express';
 import expressWS from 'express-ws';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import ngrok from 'ngrok';
+
+const PORT = 4200;
 
 const { app } = expressWS(express());
 const proxyMiddleware = createProxyMiddleware({
@@ -15,6 +18,12 @@ app.use((req, res, next) => {
 app.use('/api', api);
 app.use(proxyMiddleware);
 
-app.listen(4200, () => {
+app.listen(PORT, () => {
   console.log('Listening on http://localhost:4200');
 });
+
+if (!process.env.WEBSITE) {
+  ngrok.connect(PORT).then(result => {
+    process.env.WEBSITE = result;
+  });
+}
